d26b0897e0832cf16981ede67657e522ea6bd67c
Administrator <admin@local.host>
2017-03-27 15:45:43 +0800

    use grunt

---
+++ app/assets/js/library/MyUtil.js
@@ -0,0 +1,19 @@
+window.MyUtil = (function(){
+    var _self = this;
+
+    this.is_touch_device  = function() {
+        return 'ontouchstart' in window || 'onmsgesturechange' in window; // works on ie10
+    };
+
+    this.fbShare = function(url, title, descr, image, winWidth, winHeight) {
+        var winTop = (screen.height / 2) - (winHeight / 2);
+        var winLeft = (screen.width / 2) - (winWidth / 2);
+        window.open('http://www.facebook.com/sharer.php?s=100&p[title]=' + title + '&p[summary]=' + descr + '&p[url]=' + url + '&p[images][0]=' + image, 'sharer', 'top=' + winTop + ',left=' + winLeft + ',toolbar=0,status=0,width=' + winWidth + ',height=' + winHeight);
+    };
+
+    this.getQueryStringValue = function(key) {
+          return decodeURIComponent(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURIComponent(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));  
+    };
+
+    return _self;
+}());
